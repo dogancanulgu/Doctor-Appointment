e0e8954f3013175921e9882abc2b025af6ec1af0
Dogancan Ulgu <dogancanulgu@gmail.com>
2023-11-26 17:01:10 +0300

    Creating usermodel and route for backend of register

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 import Login from './pages/Login';
 import Register from './pages/Register';
 
 function App() {
   return (
     <BrowserRouter>
+      <Toaster position='top-center' reverseOrder={false} />
       <Routes>
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import { Button, Form, Input } from 'antd';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import toast from 'react-hot-toast';
 
 const Register = () => {
-  const onFinish = (values) => {
-    console.log(values);
+  const navigate = useNavigate();
+  const onFinish = async (values) => {
+    try {
+      const response = await axios.post('/api/user/register', values);
+      if (response.data.success) {
+        toast.success(response.data.message);
+        toast('Redirecting to login page');
+        setTimeout(() => {
+          navigate('/login');
+        }, 1000);
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      toast.error('Something went wrong');
+    }
   };
 
   return (
