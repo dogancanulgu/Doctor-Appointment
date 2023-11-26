9ce089bfacda0a0d9392c810f1e8be0f0bc587fd
Dogancan Ulgu <dogancanulgu@gmail.com>
2023-11-26 19:49:45 +0300

    Add api for login

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/userModel');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 router.post('/register', async (req, res) => {
   try {
@@ -17,13 +18,28 @@ router.post('/register', async (req, res) => {
     await newuser.save();
     res.status(200).send({ message: 'User created successfully.', success: true });
   } catch (error) {
+    console.log(error)
     res.status(500).send({ message: 'Error creating user', success: false, error });
   }
 });
 
 router.post('/login', async (req, res) => {
   try {
-  } catch (error) {}
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(200).send({ message: 'User does not exist', success: false });
+    }
+    const isMatch = await bcrypt.compare(req.body.password, user.password);
+    if (!isMatch) {
+      return res.status(200).send({ message: 'Password is incorrect', success: false });
+    } else {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
+      res.status(200).send({ message: 'Login successful', success: true, data: { token } });
+    }
+  } catch (error) {
+    console.log(error)
+    res.status(500).send({ message: 'Error logging in', success: false, error });
+  }
 });
 
 module.exports = router;
