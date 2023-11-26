9fc1ff7c3524b6fc90d10f285f8c4c3590d66a2c
Dogancan Ulgu <dogancanulgu@gmail.com>
2023-11-26 12:00:37 +0300

    React-router-dom antd bootstrap is added

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
+import { Button } from 'antd';
+
 function App() {
   return (
-    <div className="App">
+    <div className="App p-5">
       <h1>Doctor Appointment</h1>
+      <Button type="primary">Primary Button</Button>
     </div>
   );
 }
