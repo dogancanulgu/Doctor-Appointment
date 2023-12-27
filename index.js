7b26ca5b68ec5c35c26c7e47723f8e3832e3a4cc
Dogancan Ulgu <dogancanulgu@gmail.com>
2023-12-27 12:45:00 +0300

    deploy

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+// const cors = require('cors');
 const app = express();
 require('dotenv').config();
 const dbConfig = require('./config/dbConfig');
+// app.use(cors());
 app.use(express.json());
 const userRoute = require('./routes/userRoute');
 const adminRoute = require('./routes/adminRoute');
@@ -22,4 +24,5 @@ if (process.env.NODE_ENV === 'production') {
 
 const port = process.env.PORT || 5000;
 
+// app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`Node server started at port ${port}`));
